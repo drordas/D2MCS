239df9f0e43d119335a53d14e197a39e50bc0692
miferreiro <miguel.ferreiro.diaz@gmail.com>
2021-04-24 13:01:21 +0200

    Added LICENSE header
    Included the GPL-3 license header to all source files and added the package description.

--- R/prediction.MinimizeFN.R
+++ R/prediction.MinimizeFN.R
@@ -1,3 +1,27 @@
+#
+# D2MCS provides a novel framework to able to automatically develop and deploy
+# an accurate Multiple Classifier System (MCS) based on the feature-clustering
+# distribution achieved from an input dataset. D2MCS was developed focused on
+# four main aspects: (i) the ability to determine an effective method to
+# evaluate the independence of features, (ii) the identification of the optimal
+# number of feature clusters, (iii) the training and tuning of ML models and
+# (iv) the execution of voting schemes to combine the outputs of each classifier
+# comprising the MCS.
+#
+# Copyright (C) 2021 Sing Group (University of Vigo)
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <https://www.gnu.org/licenses/gpl-3.0.html>
+
 #' @title Combined metric strategy to minimize FN errors.
 #'
 #' @description Calculates if the positive class is the predicted one in any of
